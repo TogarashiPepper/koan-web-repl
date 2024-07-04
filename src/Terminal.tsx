7b77a10f4232d91f86d68d7e4583f522544e520d
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2024-07-03 22:35:32 -0700

    feat: term message and buffering

--- src/Terminal.tsx
+++ src/Terminal.tsx
@@ -3,7 +3,7 @@ import { Terminal as XTerm } from '@xterm/xterm';
 import '@xterm/xterm/css/xterm.css';
 
 // Custom theme to match style of xterm.js logo
-var baseTheme = {
+const baseTheme = {
   foreground: '#F8F8F8',
   background: '#2D2E2C',
   selection: '#5DA5D533',
@@ -24,6 +24,9 @@ var baseTheme = {
   white: '#F8F8F8',
   brightWhite: '#FFFFFF',
 };
+
+const blue = "\x1b[34m";
+const reset = "\x1b[0m"
 
 export interface TerminalProps extends ComponentProps<'div'> {}
 
@@ -50,44 +53,22 @@ export function Terminal() {
           break;
         case '\u007F':
           buffer = buffer.slice(0, -1);
+          term.write("\b \b");
           break;
         default:
-          // Print all other characters for demo
           if (
             (e >= String.fromCharCode(0x20) &&
               e <= String.fromCharCode(0x7e)) ||
             e >= '\u00a0'
           ) {
-            // command += e;
+            buffer += e;
             term.write(e);
           }
       }
     });
 
-    // Initial content.
-    term.writeln(
-      [
-        '',
-        '    Xterm.js is the frontend component that powers many terminals including',
-        ' \x1b[3mVS Code\x1b[0m, \x1b[3mHyper\x1b[0m and \x1b[3mTheia\x1b[0m!',
-        '',
-        ' ┌ \x1b[1mFeatures\x1b[0m ──────────────────────────────────────────────────────────────────┐',
-        ' │                                                                            │',
-        ' │  \x1b[31;1mApps just work                         \x1b[32mPerformance\x1b[0m                        │',
-        ' │   Xterm.js works with most terminal      Xterm.js is fast and includes an  │',
-        ' │   apps like bash, vim and tmux           optional \x1b[3mWebGL renderer\x1b[0m           │',
-        ' │                                                                            │',
-        ' │  \x1b[33;1mAccessible                             \x1b[34mSelf-contained\x1b[0m                     │',
-        ' │   A screen reader mode is available      Zero external dependencies        │',
-        ' │                                                                            │',
-        ' │  \x1b[35;1mUnicode support                        \x1b[36mAnd much more...\x1b[0m                   │',
-        ' │   Supports CJK 語 and emoji \u2764\ufe0f            \x1b[3mLinks\x1b[0m, \x1b[3mthemes\x1b[0m, \x1b[3maddons\x1b[0m,            │',
-        ' │                                          \x1b[3mtyped API\x1b[0m, \x1b[3mdecorations\x1b[0m            │',
-        ' │                                                                            │',
-        ' └────────────────────────────────────────────────────────────────────────────┘',
-        '',
-      ].join('\r\n')
-    );
+    term.writeln(`\n Welcome to the ${blue}Koan${reset} repl, type \`help\` to get started!`);
+    term.write(" λ ");
   });
 
   return <div ref={ref} style="border: 1px solid #424242; border-radius: 5px; padding: 10px" />;
