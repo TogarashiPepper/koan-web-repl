bb9b75b04f45dd0b7c46fb14e349a1b0534aa07d
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2024-07-05 02:00:02 -0700

    chore: add tla plugin so vite build works

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  plugins: [wasm(), solid()],
+  plugins: [
+    wasm(),
+    solid(),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
+  ],
 });
